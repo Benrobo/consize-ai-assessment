5a96b08f178393aa238f7236d5c21a1fea5d39c8
benrobo <alumonabenaiah71@gmail.com>
2025-04-07 18:20:05 +0100

    refactor: remove commented-out code in test file
    Clean up the test file by removing unnecessary commented-out code to improve readability and maintainability.

--- services/scraper/src/test/test.ts
+++ services/scraper/src/test/test.ts
@@ -4,20 +4,7 @@ import fs from "fs";
 import { AIModelsRouter } from "@consizeai/shared/ai-models-router";
 import env from "../config/env.js";
 
-// console.log(convertHtmlToMarkdown(htmlData));
-// fs.writeFileSync("job.md", convertHtmlToMarkdown(htmlData));
-
-// const username = "benrobo_dqnmn";
-// const password = "Benrobotut71=";
-
-// console.log(Buffer.from(`${username}:${password}`).toString("base64"));
-
 (async () => {
-  //   const resp = await indeed.scrape(
-  //     "https://www.indeed.com/jobs?q=full+stack+engineer"
-  //   );
-  //   fs.writeFileSync("job-3.md", convertHtmlToMarkdown(resp.data!));
-
   const aiRouter = new AIModelsRouter([
     {
       apiKeys: env.GEMINI_API_KEYS,
@@ -27,10 +14,4 @@ import env from "../config/env.js";
       baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
     },
   ]);
-
-  //   const response = await aiRouter.generate({
-  //     prompt: "what is TMA, SAM, SOM?",
-  //   });
-
-  //   console.log(response);
 })();
